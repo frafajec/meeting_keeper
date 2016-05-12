5c4b243e1e9e873265d739a63e44283c55b9ce92
Filip Rafajec <filip.rafajec@gmail.com>
2016-05-12 18:52:41 +0200

    [FR,AK] Layout changes, separate loading and libs
    Added new library fullcalendar, changed gulp processing options, changed
    layout and added sections for separate loading for each view out of
    layout.

---
+++ src/meeting_keeper/wwwroot/js/calendar.js
@@ -0,0 +1,39 @@
+﻿/*
+ * Calendar file
+*/
+
+$(document).ready(function () {
+
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+        dayClick: function () {
+            console.log('a day has been clicked!');
+        },
+
+        editable: true,
+        eventLimit: true,
+        selectable: true,
+        selectHelper: true,
+
+        select: function (start, end) {
+            var title = prompt('Event Title:');
+            var eventData;
+            if (title) {
+                eventData = {
+                    title: title,
+                    start: start,
+                    end: end
+                };
+                $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+            }
+            $('#calendar').fullCalendar('unselect');
+        },
+
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        }
+    })
+
+});
